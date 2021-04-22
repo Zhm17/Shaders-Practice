8ebcb3ae93e07a2f8daf43de7b95964b0e48e086
Zhemo <zhemo17@gmail.com>
2021-04-22 00:13:49 -0500

    - Teleportation

---
+++ Assets/03_VFX/04_Teleportation/SinMove.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class SinMove : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private Vector3 _startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+         _startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         transform.position = _startPosition + new Vector3(0.0f, Mathf.Sin(Time.time * speed) / 5, 0.0f);
+    }
+}
